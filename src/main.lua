d7912c904e0f53ab320bb95fc7a47d9b2f652ea8
Luis <luis@conradty.de>
2026-02-18 19:06:41 +0100

    added assets and first tests

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,16 @@
 function love.draw()
 	love.graphics.print("Hello World", 200, 300)
+	love.graphics.draw(character, 300, 200)
+end
+
+function love.load()
+	character = love.graphics.newImage("assets/Warrior_1/Idle.png")
+end
+
+function love.update(dt)
+	down = love.keyboard.isDown("t")
+	if down then
+		print("Works")
+		character.y = character.y - 10
+	end
 end
